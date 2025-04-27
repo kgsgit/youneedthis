8fe05ad90b71c26c9e9282dfe248a275acf5363f
[Iot] 김기석 <wekgs@naver.com>
2025-04-27 11:36:21 +0900

    2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,15 @@
 const menuToggle = document.getElementById('menu-toggle');
-const navUl = document.querySelector('.nav ul');
+const nav = document.getElementById('nav');
 
-menuToggle.addEventListener('click', () => {
-  navUl.classList.toggle('show');
+menuToggle.addEventListener('click', (e) => {
+  e.stopPropagation();
+  nav.querySelector('ul').classList.toggle('show');
+});
+
+// 화면 다른 곳 클릭하면 닫기
+document.addEventListener('click', (e) => {
+  const isClickInside = nav.contains(e.target) || menuToggle.contains(e.target);
+  if (!isClickInside) {
+    nav.querySelector('ul').classList.remove('show');
+  }
 });
