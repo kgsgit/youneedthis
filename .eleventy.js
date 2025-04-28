c378e464857bcf2441265a530426699c87c2be21
[Iot] 김기석 <wekgs@naver.com>
2025-04-28 22:22:51 +0900

    Add transform to strip <script/> from sitemap.xml

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,15 @@
 module.exports = function(eleventyConfig) {
-  // Copy static assets directory
+  // 1) assets 폴더 그대로 복사
   eleventyConfig.addPassthroughCopy("assets");
+
+  // 2) sitemap.xml이 나올 때만, <script/> 태그를 모두 제거하는 트랜스폼
+  eleventyConfig.addTransform("cleanSitemap", (content, outputPath) => {
+    if (outputPath && outputPath.endsWith("sitemap.xml")) {
+      return content.replace(/<script\/>\s*/g, "");
+    }
+    return content;
+  });
+
   return {
     dir: {
       input: "src",
