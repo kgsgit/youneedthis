ec8e65940465d80380b39ea495d26d60ace010c2
[Iot] 김기석 <wekgs@naver.com>
2025-04-28 21:58:33 +0900

    배포용 설정 추가

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,10 @@
-// .eleventy.js
 module.exports = function(eleventyConfig) {
-  // ① 기존 코드…
-  eleventyConfig.addPassthroughCopy("src/robots.txt");  // ← 이 줄 추가
-  eleventyConfig.addPassthroughCopy("assets");         // 이미 있으실 거예요
-
   return {
     dir: {
       input: "src",
       includes: "_includes",
       layouts: "_layouts",
       output: "_site"
-    },
-    passthroughFileCopy: true
+    }
   };
 };
