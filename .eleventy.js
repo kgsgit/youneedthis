305f38405258e809ddfbdb8d4415fb352c7f546b
[Iot] 김기석 <wekgs@naver.com>
2025-05-01 01:40:41 +0900

    수정

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,8 @@
 module.exports = function(eleventyConfig) {
-  // 1) assets 폴더 그대로 복사
   eleventyConfig.addPassthroughCopy("assets");
-  // robots.txt, ads.txt가 루트에 있으면 그대로 복사됩니다.
   eleventyConfig.addPassthroughCopy("robots.txt");
   eleventyConfig.addPassthroughCopy("ads.txt");
 
-  // 2) 빌드 후 sitemap.xml에서 <script/> 태그를 제거하는 트랜스폼
   eleventyConfig.addTransform("cleanSitemap", (content, outputPath) => {
     if (outputPath && outputPath.endsWith("/sitemap.xml")) {
       return content.replace(/<script\/>\s*/g, "");
@@ -15,10 +12,11 @@ module.exports = function(eleventyConfig) {
 
   return {
     dir: {
-      input: "src",
+-      input:    "src",
++      input:    ".",
       includes: "_includes",
-      layouts: "_layouts",
-      output: "_site"
+      layouts:   "_layouts",
+      output:    "_site"
     }
   };
 };
